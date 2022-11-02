0bc1a58f93ad72367f30c0557829d9c50638aca2
fgarrido <francisco.garrido@globalincubator.com>
2022-11-02 18:36:18 +0100

    History of moves.

--- tic-tac-toe/src/components/Board.js
+++ tic-tac-toe/src/components/Board.js
@@ -4,45 +4,18 @@ import { calculateWinner } from './Game';
 import Square from './Square';
 
 class Board extends React.Component {
+    
 
-    constructor(props){
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        };
-    }
-
-    handleClick(i){
-        const squares = this.state.squares.slice();
-        
-        if(calculateWinner(squares) || squares[i]){return;}
-
-        squares[i] = this.state.xIsNext?'X':'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext
-        });
-        
-    }
+    
 
     renderSquare(i) {
         return <Square
          value={this.state.squares[i]}
-         onClick={() => this.handleClick(i)}
+         onClick={() => this.props.onClick(i)}
         />;
     }
 
     render(){
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if(winner) {
-            status = 'Winner: ' + winner;
-        }else{
-            status = 'Next player: ' + (this.state.xIsNext?'X':'O');
-        }
-
-        
         return (
             <div>
                 <div className='status'>{status}</div>
